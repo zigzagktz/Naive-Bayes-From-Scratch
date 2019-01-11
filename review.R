d799f6039456f69ff1edc6b746ee62603c52984c
Kshitiz Sirohi <32847030+zigzagktz@users.noreply.github.com>
2019-01-10 23:37:28 -0500

    update

--- review.R
+++ review.R
@@ -11,12 +11,12 @@ var <- cbind(var,condition)
 var <- aggregate(var[,1:ncol(var)-1],by=list(var$condition),sum)
 colnames(var)[1] <- "condition"
 
-d <- "was so happy that I fell in love with the movie"
-d <- strsplit(d," ")
+
+  d <- strsplit(d," ")
 d <- unlist(d)
 
-negative <- var[which(var$condition=="N"),]
-positive <- var[which(var$condition=="P"),]
+negative <- var[which(var$condition=="neg"),]
+positive <- var[which(var$condition=="pos"),]
 
 n_sum <-  rowSums(negative[,2:ncol(negative)])
 p_sum <-  rowSums(positive[,2:ncol(positive)])
@@ -38,23 +38,13 @@ positive_prob <- t(positive_prob)
 
 values <- match(d,colnames(negative_prob))
 values <- values[!is.na(values)]
-
  probs_neg <- negative_prob[,values]
  probs_pos <- positive_prob[,values]
-
-##probs <- rbind(positive_prob,negativ_prob)
-##probs <- round(bar,2)
-
-##values <- match(d,colnames(var[,2:ncol(var)]))
-##values <- values[!is.na(values)]
-  
-#probs <- probs[,values]
-
-
 positive_rev <- prob_p * prod(probs_pos) *100
 negative_rev <- prob_n * prod(probs_neg) * 100
-
-positive_rev
-negative_rev
+ positive_rev
+ negative_rev
 a <- positive_rev > negative_rev
 ifelse(a, print("positive"),print("negative"))
+
+
